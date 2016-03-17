8d571105bef1bcea4a191f0de932c3cbc694f171
lprice <lprice@saintjoe.edu>
2016-03-17 12:53:55 -0400

    size mods

--- DogLCP/src/edu/saintjoe/lprice/Dog.java
+++ DogLCP/src/edu/saintjoe/lprice/Dog.java
@@ -34,10 +34,17 @@ public class Dog {
 }
 
 public void bark() {
-	System.out.println(name = "says Ruff!");	}
-			
-
+	if (size > 60) {
+	System.out.println("Woof! Woof!");
+	} else if (size > 14) {
+		System.out.println("Ruff! Ruff!);
+	} else {
+		System.out.println("Yip! Yip!");
+	}
 }
+}
+				
+
 public void eat() { }
 public void chaseCat() { }
 )
