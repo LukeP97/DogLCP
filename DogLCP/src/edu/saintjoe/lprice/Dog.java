c1c2c9d4ce5c61d2eb2c870cb488562f8d288217
lprice <lprice@saintjoe.edu>
2016-03-16 23:08:22 -0400

    finish code

--- DogLCP/src/edu/saintjoe/lprice/Dog.java
+++ DogLCP/src/edu/saintjoe/lprice/Dog.java
@@ -1,5 +1,43 @@
 package edu.saintjoe.lprice;
 
 public class Dog {
+	String name;
+	public static void main (String[] args) 
+	// make a Dog object and access it
+	Dog dog1 = new dog();
+	dog1.bark(); 
+	dog1.name = "Bart";
+	
+	//now make a Dog array 
+	Dog[] myDogs = new Dog[3];
+	// and put some dogs in it 
+	myDogs[0] = new Dog();
+	myDogs[1] = new Dog();
+	myDogs[2] = dog1;
+	
+	// now access the Dogs using the array
+	// refrences 
+	myDogs[0].name = "Fred";
+	myDogs[1].name = "Marge"; 
+	
+	// Hmmmm... what is myDogs[2] name?
+	System.out.print("last dog's name is");
+			System.out.println(myDogs[2].name);
+	
+	//now loop through the arrray 
+	//and tell all dogs to bark
+	int x = 0;
+	while(x < myDogs) {
+		myDogs[x].bark();
+		x = x + 1;
+		}
+}
+
+public void bark() {
+	System.out.println(name = "says Ruff!");	}
+			
 
 }
+public void eat() { }
+public void chaseCat() { }
+)
